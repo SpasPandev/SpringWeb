790d713df7698224413dff4805a6679c73c6e5a6
SpasPandev <sppan@abv.bg>
2023-03-30 13:37:07 +0300

    Build skeleton for services and repositories - CoffeeShop project
    CoffeeShop project

---
+++ CoffeeShop/src/main/java/com/example/coffeeshop/service/Impl/CategoryServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.coffeeshop.service.Impl;
+
+import com.example.coffeeshop.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+}
---
+++ CoffeeShop/src/main/java/com/example/coffeeshop/service/Impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.coffeeshop.service.Impl;
+
+import com.example.coffeeshop.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+}
