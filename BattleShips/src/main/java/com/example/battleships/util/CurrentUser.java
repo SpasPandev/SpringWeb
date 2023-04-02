215a6da0037d9808b920463b97e71ad2a7ed3009
SpasPandev <sppan@abv.bg>
2023-04-02 19:37:10 +0300

    Added current user session and separated the important files into a fragments folder - BattleShips project
    BattleShips project

---
+++ BattleShips/src/main/java/com/example/battleships/util/CurrentUser.java
@@ -0,0 +1,35 @@
+package com.example.battleships.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@SessionScope
+@Component
+public class CurrentUser {
+
+    private Long id;
+    private String username;
+
+    public CurrentUser() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isLoggedIn() {
+        return id != null;
+    }
+}
