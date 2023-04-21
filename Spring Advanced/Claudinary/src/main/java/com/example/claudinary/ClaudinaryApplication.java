e92d87dfa5e91ddd6d178cc57161fc6d6ebb82d1
SpasPandev <sppan@abv.bg>
2023-04-21 17:50:24 +0300

    Setup project - Claudinary demo project
    Claudinary demo project

---
+++ Spring Advanced/Claudinary/src/main/java/com/example/claudinary/ClaudinaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.claudinary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudinaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudinaryApplication.class, args);
+	}
+
+}
---
+++ Spring Advanced/Claudinary/src/test/java/com/example/claudinary/ClaudinaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.claudinary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaudinaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
