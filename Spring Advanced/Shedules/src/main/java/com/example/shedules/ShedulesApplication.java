33cd4cac7ce29da56593d465232296b3e634d6fa
SpasPandev <sppan@abv.bg>
2023-05-03 11:45:46 +0300

    Setup project - Schedules demo
    Schedules demo

---
+++ Spring Advanced/Shedules/src/main/java/com/example/shedules/ShedulesApplication.java
@@ -0,0 +1,13 @@
+package com.example.shedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShedulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShedulesApplication.class, args);
+    }
+
+}
