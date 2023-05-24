1a212ca2fa33155cba7cb5d5a718aa7b957f789c
SpasPandev <sppan@abv.bg>
2023-05-24 19:45:07 +0300

    Example how to consume REST API from a spring backend - BooksClient project
    BooksClient project

---
+++ Spring Advanced/BooksClient/src/main/java/com/example/booksclient/model/AuthorDTO.java
@@ -0,0 +1,29 @@
+package com.example.booksclient.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AuthorDTO {
+
+    private String name;
+
+    public AuthorDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorDTO{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ Spring Advanced/BooksClient/src/main/java/com/example/booksclient/model/BookDTO.java
@@ -0,0 +1,62 @@
+package com.example.booksclient.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BookDTO {
+
+    private Long id;
+
+    private String title;
+
+    private String isbn;
+
+    private AuthorDTO author;
+
+    public BookDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public AuthorDTO getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDTO author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "BookDTO{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", author=" + author +
+                '}';
+    }
+}
