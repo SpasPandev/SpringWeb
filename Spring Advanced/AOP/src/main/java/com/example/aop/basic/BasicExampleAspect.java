bcb70aaa3f9b5835599f24ca8c176ad6a7e83c80
SpasPandev <sppan@abv.bg>
2023-05-24 16:08:26 +0300

    Examples with @Aspect , @Pointcut , @Before and @AfterThrowing - AOP demo
    AOP demo

---
+++ Spring Advanced/AOP/src/main/java/com/example/aop/basic/BasicExampleAspect.java
@@ -0,0 +1,38 @@
+package com.example.aop.basic;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class BasicExampleAspect {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(BasicExampleAspect.class);
+
+    @Pointcut("execution(* com.example.aop.Student.*(..))")
+    public void track() {};
+
+    @Pointcut("execution(* com.example.aop.Student.echo(..))")
+    public void trackEcho() {};
+
+    @Before("track()")
+    public void beforeAnyMethod(JoinPoint joinPoint) {
+        LOGGER.info("Before calling: {}", joinPoint.getSignature());
+    }
+
+    @Before("trackEcho()")
+    public void beforeEcho() {
+        LOGGER.info("Advise execution before calling echo.");
+    }
+
+    @AfterThrowing(pointcut = "track()", throwing = "error")
+    public void trackExceptions(Throwable error) {
+        LOGGER.info("I have detected an exception: ", error);
+    }
+}
